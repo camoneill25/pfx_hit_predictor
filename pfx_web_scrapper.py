13d1228bd2d17f1f8d390ba1127bd97402cc3ba2
camoneill25 <oneill.cameron@outlook.com>
2018-01-24 11:19:49 -0800

    Added SQL Query

--- pfx_web_scrapper.py
+++ pfx_web_scrapper.py
@@ -11,3 +11,13 @@ for url in http_request_urls:
         continue
     else:
         pitcher_df = pd.read_html(read_html_str, header=0)
+        pitcher_df.to_csv('csv_pitcher_table.csv', sep='\t', encoding='utf-8')
+        with open ('test.csv', 'r') as f:
+            reader = csv.reader(f)
+            columns = next(reader)
+            query = "INSERT INTO PitchByPitch({0}) values ({1})"
+            query = query.format(','.join(columns), ','.join('?' * len(columns)))
+            cursor = connection.cursor()
+            for data in reader:
+                cursor.execute(query, data)
+            cursor.commit()
